4f08e7bad447a023d320bfe8730e80993febed64
felipemiad <jf.cortesc1@uniandes.edu.co>
2024-11-23 18:42:53 -0500

    reentrenando el modelo

--- model_pkg/predictor.py
+++ model_pkg/predictor.py
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-=======
-import numpy as np
->>>>>>> 66eca488ff3c8b50058709080352271dbb2ac608
-import pandas as pd
 import joblib
+import pandas as pd
 from typing import Dict, Any
-from loguru import logger
 
-<<<<<<< HEAD
-# Rutas de los archivos del modelo y encoder
-MODEL_PATH = "C:/Users/johan/Documents/Maestria/Despliegue Analitica/ProyectoIcfes/icfes-api/model_pkg/model_icfes.pkl"  # Ruta del modelo
-ENCODER_PATH = "C:/Users/johan/Documents/Maestria/Despliegue Analitica/ProyectoIcfes/icfes-api/model_pkg/encoder_icfes.pkl"  # Ruta del encoder
-=======
-# Ruta del modelo previamente entrenado
-MODEL_PATH = "model_pkg/model_icfes.pkl"
-logger.info(f"Cargando el modelo desde {MODEL_PATH}...")
+# Ruta del modelo y encoder
+MODEL_PATH = "C:/Users/johan\Documents/Maestria/Despliegue Analitica/ProyectoIcfes/icfes-api/model_pkg/model_icfes.pkl"
+ENCODER_PATH = "C:/Users/johan\Documents/Maestria/Despliegue Analitica/ProyectoIcfes/icfes-api/model_pkg/encoder_icfes.pkl"
+
+# Cargar modelo y encoder
 model = joblib.load(MODEL_PATH)
-logger.info("Modelo cargado correctamente.")
->>>>>>> 66eca488ff3c8b50058709080352271dbb2ac608
+encoder = joblib.load(ENCODER_PATH)
 
-# Variables finales necesarias para el modelo
 VARIABLES_FINALES = [
     "FAMI_ESTRATOVIVIENDA",
     "FAMI_PERSONASHOGAR",
@@ -32,14 +22,10 @@ VARIABLES_FINALES = [
     "COLE_GENERO",
 ]
 
-# Cargar el modelo y el encoder
-model = joblib.load(MODEL_PATH)
-encoder = joblib.load(ENCODER_PATH)
-
 def make_prediction(input_data: pd.DataFrame) -> Dict[str, Any]:
     """
     Realiza una predicción usando el modelo cargado.
-    
+
     Parameters
     ----------
     input_data : pd.DataFrame
@@ -53,39 +39,20 @@ def make_prediction(input_data: pd.DataFrame) -> Dict[str, Any]:
     result = {"predictions": None, "errors": None}
 
     try:
-        logger.info("Validando las columnas en los datos de entrada...")
-        # Validar que las columnas necesarias están presentes en el DataFrame
+        # Validar que las columnas necesarias están presentes
         missing_cols = set(VARIABLES_FINALES) - set(input_data.columns)
         if missing_cols:
-            result["errors"] = f"Faltan las columnas requeridas: {missing_cols}"
-<<<<<<< HEAD
+            result["errors"] = f"Faltan columnas requeridas: {missing_cols}"
             return result
-
-        # Ordenar las columnas según VARIABLES_FINALES
-        input_data = input_data[VARIABLES_FINALES]
 
         # Codificar los datos de entrada
-        input_encoded = encoder.transform(input_data)
-
-        # Realizar las predicciones
-        predictions = model.predict(input_encoded)
-=======
-            logger.error(result["errors"])
-            return result
-
-        # Reordenar las columnas para que coincidan con el orden esperado por el modelo
-        input_data = input_data[VARIABLES_FINALES]
-        logger.info(f"Datos después de validar y reordenar: {input_data.head()}")
+        input_encoded = encoder.transform(input_data[VARIABLES_FINALES])
 
         # Realizar predicciones
-        logger.info("Realizando predicción con el modelo...")
-        predictions = model.predict(input_data)
->>>>>>> 66eca488ff3c8b50058709080352271dbb2ac608
+        predictions = model.predict(input_encoded)
         result["predictions"] = predictions.tolist()
-        logger.info(f"Predicciones realizadas con éxito: {result['predictions']}")
 
     except Exception as e:
         result["errors"] = str(e)
-        logger.error(f"Error durante la predicción: {str(e)}")
 
     return result
