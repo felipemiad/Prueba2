635d8752d6d239b280523d8f9abcd5f7edfff010
felipemiad <jf.cortesc1@uniandes.edu.co>
2024-11-23 17:18:20 -0500

    configuracion config

--- app/config.py
+++ app/config.py
@@ -4,10 +4,18 @@ from loguru import logger
 from pydantic import BaseSettings
 
 
-# Configuración básica de la aplicación
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "ICFES API"
     API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "API de Predicciones ICFES"
+
+    # Configuración de CORS
+    BACKEND_CORS_ORIGINS: list[str] = [
+        "http://localhost",
+        "http://localhost:8000",
+        "http://localhost:3000",
+        "https://localhost:8000",
+        "https://localhost:3000",
+    ]  # Ajusta estos valores según las necesidades
 
     class Config:
         case_sensitive = True
