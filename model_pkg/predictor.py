ef91e74966b463af6e439bf87b9e40140fe4fb77
felipemiad <jf.cortesc1@uniandes.edu.co>
2024-11-23 18:26:25 -0500

    ajustando encoder

--- model_pkg/predictor.py
+++ model_pkg/predictor.py
@@ -1,15 +1,12 @@
-import joblib
 import pandas as pd
+import joblib
 from typing import Dict, Any
 
-# Ruta del modelo y encoder
-MODEL_PATH = "model_pkg/model_icfes.pkl"
-ENCODER_PATH = "model_pkg/encoder_icfes.pkl"
+# Rutas de los archivos del modelo y encoder
+MODEL_PATH = "C:/Users/johan/Documents/Maestria/Despliegue Analitica/ProyectoIcfes/icfes-api/model_pkg/model_icfes.pkl"  # Ruta del modelo
+ENCODER_PATH = "C:/Users/johan/Documents/Maestria/Despliegue Analitica/ProyectoIcfes/icfes-api/model_pkg/encoder_icfes.pkl"  # Ruta del encoder
 
-# Cargar modelo y encoder
-model = joblib.load(MODEL_PATH)
-encoder = joblib.load(ENCODER_PATH)
-
+# Variables finales necesarias para el modelo
 VARIABLES_FINALES = [
     "FAMI_ESTRATOVIVIENDA",
     "FAMI_PERSONASHOGAR",
@@ -22,10 +19,14 @@ VARIABLES_FINALES = [
     "COLE_GENERO",
 ]
 
+# Cargar el modelo y el encoder
+model = joblib.load(MODEL_PATH)
+encoder = joblib.load(ENCODER_PATH)
+
 def make_prediction(input_data: pd.DataFrame) -> Dict[str, Any]:
     """
     Realiza una predicción usando el modelo cargado.
-
+    
     Parameters
     ----------
     input_data : pd.DataFrame
@@ -42,13 +43,16 @@ def make_prediction(input_data: pd.DataFrame) -> Dict[str, Any]:
         # Validar que las columnas necesarias están presentes
         missing_cols = set(VARIABLES_FINALES) - set(input_data.columns)
         if missing_cols:
-            result["errors"] = f"Faltan columnas requeridas: {missing_cols}"
+            result["errors"] = f"Faltan las columnas requeridas: {missing_cols}"
             return result
 
-        # Codificar los datos de entrada
-        input_encoded = encoder.transform(input_data[VARIABLES_FINALES])
+        # Ordenar las columnas según VARIABLES_FINALES
+        input_data = input_data[VARIABLES_FINALES]
 
-        # Realizar predicciones
+        # Codificar los datos de entrada
+        input_encoded = encoder.transform(input_data)
+
+        # Realizar las predicciones
         predictions = model.predict(input_encoded)
         result["predictions"] = predictions.tolist()
 
