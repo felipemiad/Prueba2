caf6a1217b79b578cdbe395abc48acb0deb9767d
felipemiad <jf.cortesc1@uniandes.edu.co>
2024-11-23 17:04:12 -0500

    Actualizacion de config

--- app/config.py
+++ app/config.py
@@ -1,11 +1,28 @@
+import logging
+import sys
+from loguru import logger
 from pydantic import BaseSettings
 
+
+# Configuración básica de la aplicación
 class Settings(BaseSettings):
     PROJECT_NAME: str = "ICFES API"
-    MODEL_VERSION: str = "1.0.0"
+    API_V1_STR: str = "/api/v1"
 
     class Config:
         case_sensitive = True
 
 
+# Función para configurar los logs
+def setup_app_logging():
+    """Configura el logger para la aplicación."""
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        stream=sys.stdout,
+    )
+    logger.info("Logging configurado correctamente.")
+
+
+# Instancia de configuración
 settings = Settings()
