93ef19b026c6d735141413cb42e9ea26747f6341
felipemiad <jf.cortesc1@uniandes.edu.co>
2024-11-23 17:35:26 -0500

    modificacion de api para manejo de errores

--- app/api.py
+++ app/api.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
+from loguru import logger
 import pandas as pd
-import numpy as np
 from model_pkg.predictor import make_prediction
 
-# Definir los esquemas de entrada y salida
+# Esquemas de entrada y salida
 class MultipleDataInputs(BaseModel):
     inputs: list[dict]
 
@@ -29,21 +29,26 @@ async def predict(input_data: MultipleDataInputs):
     """
     Realiza predicciones usando el modelo Gradient Boosting.
     """
+    logger.info("Recibiendo datos de entrada para predicción.")
     try:
         # Convertir los datos de entrada en un DataFrame
         input_df = pd.DataFrame(jsonable_encoder(input_data.inputs))
+        logger.info(f"Datos de entrada: {input_df.to_dict(orient='records')}")
 
         # Llamar a la función `make_prediction` del predictor
         results = make_prediction(input_df)
 
         if results["errors"]:
+            logger.error(f"Errores durante la predicción: {results['errors']}")
             raise HTTPException(status_code=400, detail=results["errors"])
 
+        logger.info(f"Predicciones generadas: {results['predictions']}")
         return {
             "errors": None,
             "version": "1.0.0",
             "predictions": results["predictions"],
         }
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
 
+    except Exception as e:
+        logger.error(f"Error inesperado: {str(e)}")
+        raise HTTPException(status_code=500, detail="Error durante la predicción")
