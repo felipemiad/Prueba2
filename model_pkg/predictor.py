78448eecd9111551d31d9eb372e449c34d24f2d4
felipemiad <jf.cortesc1@uniandes.edu.co>
2024-11-23 19:16:36 -0500

    agregando log para evidenciar errores

--- model_pkg/predictor.py
+++ model_pkg/predictor.py
@@ -1,16 +1,15 @@
-import joblib
 import pandas as pd
+import joblib
 from typing import Dict, Any
+from loguru import logger
 
-# Ruta del modelo y encoder
+# Cargar el modelo y el encoder
 MODEL_PATH = "model_pkg/model_icfes.pkl"
 ENCODER_PATH = "model_pkg/encoder_icfes.pkl"
-
-
-# Cargar modelo y encoder
 model = joblib.load(MODEL_PATH)
 encoder = joblib.load(ENCODER_PATH)
 
+# Variables finales necesarias para el modelo
 VARIABLES_FINALES = [
     "FAMI_ESTRATOVIVIENDA",
     "FAMI_PERSONASHOGAR",
@@ -25,7 +24,7 @@ VARIABLES_FINALES = [
 
 def make_prediction(input_data: pd.DataFrame) -> Dict[str, Any]:
     """
-    Realiza una predicción usando el modelo cargado.
+    Realiza una predicción usando el modelo y el encoder cargados.
 
     Parameters
     ----------
@@ -40,20 +39,35 @@ def make_prediction(input_data: pd.DataFrame) -> Dict[str, Any]:
     result = {"predictions": None, "errors": None}
 
     try:
-        # Validar que las columnas necesarias están presentes
+        logger.info("Validando las columnas en los datos de entrada...")
+        # Validar que las columnas necesarias están presentes en el DataFrame
         missing_cols = set(VARIABLES_FINALES) - set(input_data.columns)
         if missing_cols:
-            result["errors"] = f"Faltan columnas requeridas: {missing_cols}"
+            result["errors"] = f"Faltan las columnas requeridas: {missing_cols}"
+            logger.error(result["errors"])
             return result
 
-        # Codificar los datos de entrada
-        input_encoded = encoder.transform(input_data[VARIABLES_FINALES])
+        logger.info(f"Datos originales:\n{input_data.head()}")
+        logger.info(f"Tipos de datos originales:\n{input_data.dtypes}")
+
+        # Asegurarse de que los tipos de datos son consistentes
+        for col in VARIABLES_FINALES:
+            input_data[col] = input_data[col].astype(str)
+
+        logger.info(f"Datos después de transformar a tipo 'str':\n{input_data.head()}")
+        logger.info(f"Tipos de datos después de transformar:\n{input_data.dtypes}")
+
+        # Codificar las variables categóricas
+        input_data_encoded = encoder.transform(input_data[VARIABLES_FINALES])
+        logger.info(f"Datos codificados:\n{input_data_encoded}")
 
         # Realizar predicciones
-        predictions = model.predict(input_encoded)
+        logger.info("Realizando predicción con el modelo...")
+        predictions = model.predict(input_data_encoded)
         result["predictions"] = predictions.tolist()
 
     except Exception as e:
         result["errors"] = str(e)
+        logger.error(f"Error durante la predicción: {str(e)}")
 
     return result
